d2a32b7e4fb794e24b059f4d9f6f28e6de6421f6
Rajesh Madasu <rajesh.madasu@nagarro.com>
2022-12-21 10:45:40 +0400

    change from ListView to ListView.Builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,49 +11,48 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView(
-          children: transactions.map((tx) {
-            return Container(
-                width: double.infinity,
-                child: Card(
-                  margin: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 15.0),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-                                style: BorderStyle.solid)),
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.purple,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tx.title,
-                              style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.start),
-                          Text(DateFormat.yMMMEd().format(tx.date),
-                              style: const TextStyle(
-                                  fontSize: 14, color: Colors.grey)),
-                        ],
-                      )
-                    ],
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              margin: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                            style: BorderStyle.solid)),
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      '\$${transactions[index].amount}',
+                      style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ));
-          }).toList(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(transactions[index].title,
+                          style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.start),
+                      Text(DateFormat.yMMMEd().format(transactions[index].date),
+                          style: const TextStyle(
+                              fontSize: 14, color: Colors.grey)),
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,
         ));
   }
 }
